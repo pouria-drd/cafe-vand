5661d98864fe560b3d7b8e1d1b22ec61c79deaba
pouria-drd <pouriadrd@gmail.com>
2024-09-06 20:39:56 +0330

    moved category table to category page

--- app/vand-panel/categories/page.tsx
+++ app/vand-panel/categories/page.tsx
@@ -1,5 +1,17 @@
-function CategoriesPage() {
-    return <div>CategoriesPage</div>;
+import { getPanelCategories } from "@/actions";
+import { CategoryTable } from "@/components/ui";
+
+async function CategoriesPage() {
+    // Fetch categories from the server
+    const result = await getPanelCategories();
+
+    return (
+        <section className="">
+            <div>VandPanel</div>
+
+            <CategoryTable categories={result?.data} error={result?.error} />
+        </section>
+    );
 }
 
 export default CategoriesPage;
--- app/vand-panel/page.tsx
+++ app/vand-panel/page.tsx
@@ -1,15 +1,7 @@
-import { getPanelCategories } from "@/actions";
-import { CategoryTable } from "@/components/ui";
-
 async function VandPanel() {
-    // Fetch categories from the server
-    const result = await getPanelCategories();
-
     return (
         <section className="">
             <div>VandPanel</div>
-
-            <CategoryTable categories={result?.data} error={result?.error} />
         </section>
     );
 }
